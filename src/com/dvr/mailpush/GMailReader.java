8f6ea34a5a07ce9ac9e1e2b41ef4a5decd63f511
Jonny rojas <jrr1047@gmail.com>
2014-07-26 18:25:30 -0500

    Corriendo la busqueda de correos en imbox Gmail

--- src/com/dvr/mailpush/GMailReader.java
+++ src/com/dvr/mailpush/GMailReader.java
@@ -3,7 +3,6 @@
  */
 package com.dvr.mailpush;
 
-
 import java.util.Properties;
 
 import javax.mail.Folder;
@@ -15,86 +14,57 @@ import javax.mail.Store;
 
 import android.util.Log;
 
+public class GMailReader extends javax.mail.Authenticator {
+	private static final String TAG = "GMailReader";
+	private static final String TAG1 = "jonny";
 
-public class GMailReader extends javax.mail.Authenticator { 
-    private static final String TAG = "jonny";
+	private String mailhost = "imap.gmail.com";
+	private Session session;
+	private Store store;
 
-    private String mailhost = "imaps.gmail.com";  
-    private Session session;
-    private Store store;
-    private String Usuari;
-    private String Password;
-    
-    public  GMailReader(String user, String  password) {
-		Usuari = user;
-		Password = password;
-        Properties props = System.getProperties();
-        if (props == null){
-        	Log.e(TAG, "Properties are null !!");
-        } else {
-	        props.setProperty("mail.store.protocol", "imaps");
-	        props.setProperty("mail.imap.host", mailhost);
-	        props.setProperty("mail.imap.port", "993");
-	        props.setProperty("mail.imap.sasl.enable", "true");
-	        props.setProperty("mail.imap.sasl.mechanisms", "XOAUTH2");
-	        props.setProperty("mail.imap.auth.login.disable", "true");
-	        props.setProperty("mail.imap.auth.plain.disable", "true");
-	        props.setProperty("mail.imap.ssl.enable", "true");
-	
-	        Log.d(TAG, "Transport: "+props.getProperty("mail.transport.protocol"));
-	        Log.d(TAG, "Store: "+props.getProperty("mail.store.protocol"));
-	        Log.d(TAG, "Host: "+props.getProperty("mail.imap.host"));
-	        Log.d(TAG, "Authentication: "+props.getProperty("mail.imap.auth"));
-	        Log.d(TAG, "Port: "+props.getProperty("mail.imap.port"));
-	        Log.d(TAG, "SASL: "+props.getProperty("mail.imap.sasl.enable"));
-	        Log.d(TAG, "SASL Mec: "+props.getProperty("mail.imap.sasl.mechanisms"));
-	        Log.d(TAG, "Login Disable: "+props.getProperty("mail.imap.auth.login.disable"));
-	        Log.d(TAG, "Plain Disable: "+props.getProperty("mail.imap.auth.plain.disable"));
-	        Log.d(TAG, "SSL Enable: "+props.getProperty("mail.imap.ssl.enable"));
-        }
-        
-       
-        
-	    try {
-	        session = Session.getDefaultInstance(props, null);
-	        store = session.getStore("imaps");
-	        store.connect(mailhost, Usuari, Password);
-	        Log.i(TAG, "Store: "+store.toString());
-	    } catch (NoSuchProviderException e) {
-	        // TODO Auto-generated catch block
-	        e.printStackTrace();
-	    } catch (MessagingException e) {
-	        // TODO Auto-generated catch block
-	        e.printStackTrace();
-	    }
-    }
-    
-    public synchronized int readMail() throws Exception { 
-        try { 
-            Folder folder = store.getFolder("INBOX"); 
-            folder.open(Folder.READ_ONLY);
-            Message [] mensajes = folder.getMessages();
-            for (int i=0;i<mensajes.length;i++)
-            {
-            	Log.d(TAG,"From:"+mensajes[i].getFrom()[0].toString());
-            	Log.d(TAG,"Subject:"+mensajes[i].getSubject());
-            }
+	public GMailReader(String user, String password) {
 
-            /* TODO to rework
-            Message[] msgs = folder.getMessages(1, 10);
-            FetchProfile fp = new FetchProfile(); 
-            fp.add(FetchProfile.Item.ENVELOPE); 
-            folder.fetch(msgs, fp);
-            
-            Message[] msgs = folder.getMessages();
-            return msgs;*/
-             
-            return folder.getUnreadMessageCount();
-            
-        } catch (Exception e) { 
-            Log.e(TAG, e.getMessage(), e); 
-            return 0; 
-        } 
-    } 
-    
+		Properties props = System.getProperties();
+		if (props == null) {
+			Log.e(TAG, "Properties are null !!");
+		} else {
+			props.setProperty("mail.store.protocol", "imaps");
+
+			Log.d(TAG,"Transport: "+ props.getProperty("mail.transport.protocol"));
+			Log.d(TAG, "Store: " + props.getProperty("mail.store.protocol"));
+			Log.d(TAG, "Host: " + props.getProperty("mail.imap.host"));
+			Log.d(TAG, "Authentication: " + props.getProperty("mail.imap.auth"));
+			Log.d(TAG, "Port: " + props.getProperty("mail.imap.port"));
+		}
+		try {
+			session = Session.getDefaultInstance(props, null);
+			store = session.getStore("imaps");
+			store.connect(mailhost, user, password);
+			Log.i(TAG, "Store: " + store.toString());
+		} catch (NoSuchProviderException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public synchronized void readMail() throws Exception {
+		try {
+			Folder folder = store.getFolder("Inbox");
+			folder.open(Folder.READ_ONLY);
+			Message [] mensajes = folder.getMessages();
+			for (int i=0;i<mensajes.length;i++)
+			{
+				Log.d(TAG1,"From:"+mensajes[i].getFrom()[0].toString());
+				Log.d(TAG1,"Subject:"+mensajes[i].getSubject());
+			}
+			
+
+		} catch (Exception e) {
+			Log.e("readMail", e.getMessage(), e);
+
+		}
+	}
 }
--- src/com/dvr/mailpush/MainActivity.java
+++ src/com/dvr/mailpush/MainActivity.java
@@ -1,8 +1,8 @@
 package com.dvr.mailpush;
 
 
-
 import android.support.v7.app.ActionBarActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -10,14 +10,27 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-	private static final String TAG = "probando";
 
+	private static final String TAG = "gmail";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       GMailReader jj = new GMailReader("jrr1047@gmail.com","1047jonny");
         
+        
+        new AsyncTask<Void, Void, Void>() {
+			@Override
+			protected Void doInBackground(Void... params) {
+				final GMailReader leer = new GMailReader("jrr1047@gmail.com", "1047jonny");
+				try {
+					leer.readMail();
+					this.cancel(true);
+				} catch (Exception e) {
+					Log.e(TAG, e+"");
+				}
+				return null;
+			}
+		}.execute();
     }
 
 
